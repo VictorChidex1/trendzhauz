ca139a28c47645d247c81a8977c3acd03ec8face
“VictorChidex1” <Donchid.online@gmail.com>
2026-08-08 21:48:54 +0100

    fix(build): remove unused React imports causing TS6133 on Vercel

--- src/pages/PrivacyPolicyPage.tsx
+++ src/pages/PrivacyPolicyPage.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+
 import { Shield, Sparkles, ArrowRight } from "lucide-react";
 import { Link } from "react-router-dom";
 
--- src/pages/TermsOfServicePage.tsx
+++ src/pages/TermsOfServicePage.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+
 import { Scale, Sparkles, ArrowRight } from "lucide-react";
 import { Link } from "react-router-dom";
 
